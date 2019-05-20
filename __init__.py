1c76292533cff0958ed24507c03bfda9412b4fcc
Andy Johnson <41394292+AndyJ30@users.noreply.github.com>
2019-05-20 10:59:27 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .airMonitor import AirMonitor
+from .airMonitor import AirMonitorException
+from .airMonitor import SensorType
+from .airMonitor import TimeConvention
+from .airMonitor import AveragePeriod
